c5d9d4000ecc77385d23a4d6883e809632ea694f
Chyngyz <konuratov.chika@gmail.com>
2023-06-30 18:57:22 +0600

    Поправил

--- Source/webapp/urls.py
+++ Source/webapp/urls.py
@@ -5,6 +5,6 @@ from webapp.views import todolist_view, todolist_detail, todolist_delete, todoli
 urlpatterns = [
     path('', todolist_view, name='todolist'),
     path('detail/<int:pk>', todolist_detail, name='detail'),
-    path('detail/delete/', todolist_delete, name='delete'),
+    path('detail/<int:pk>/delete/', todolist_delete, name='delete'),
     path('create/', todolist_create, name='create')
 ]
